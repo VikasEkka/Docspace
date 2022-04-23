be2bd75077f5bc7ed74ac422df32ef62fe613ac8
VikasEkka <vikasekka08@gmail.com>
2022-04-23 18:30:53 +0530

    logout added

---
+++ src/components/authentication/logoutButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Logout = () => {
+    const {logout,isAuthenticated}={useAuth0};
+    return ( 
+        isAuthenticated && (
+        <div>
+            <button onClick={()=>logout()}>Log out</button>
+        </div>
+        )
+     );
+}
+
+export default Logout;
